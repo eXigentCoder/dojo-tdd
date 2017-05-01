86384fd274dd9d7bb05efe9cb352bd6c7c6877af
Ryan Kotzen <ryan@encentivize.co.za>
2017-05-01 19:05:39 +0200

    Refactoring base route to its own file so that as it grows we won't clog up our routing logic.

--- src/routes/base.js
+++ src/routes/base.js
@@ -1,6 +1,7 @@
 'use strict';
 const router = require('express').Router({mergeParams: true});
-const baseRouter = require('./base');
 module.exports = router;
 
-router.use('/', baseRouter);
+router.get('/', function (req, res) {
+    res.json();
+});
