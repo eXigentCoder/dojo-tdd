73f7a26c590cd279c552e7cfc62ee8cc9eee8f1f
Ryan Kotzen <ryan@encentivize.co.za>
2017-05-01 20:25:13 +0200

    Green

--- src/routes/base.js
+++ src/routes/base.js
@@ -1,7 +1,10 @@
 'use strict';
 const router = require('express').Router({mergeParams: true});
 module.exports = router;
+const packageJson = require('../../package.json');
 
 router.get('/', function (req, res) {
-    res.json();
+    res.json({
+        name: packageJson.name
+    });
 });
