a3e172824c17b3f2bbd460255040c46ff43d2d5a
Ryan Kotzen <ryan@encentivize.co.za>
2017-05-05 09:28:45 +0200

    adding missing routes registration.

--- src/create-app.js
+++ src/create-app.js
@@ -1,7 +1,8 @@
 'use strict';
 const express = require('express');
-
+const routes = require('./routes/index');
 module.exports = function createApp(callback) {
     const app = express();
+    app.use(routes);
     return callback(null, app);
 };
